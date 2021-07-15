6f4055e85664cda61aae6184784df146f5a8eaab
GabrielIFPB <jeronimogbariel@gmail.com>
2021-07-15 16:15:14 -0300

    add url detail

--- src/main/java/com/inteligenciadigital/codeblog/controllers/CodeblogController.java
+++ src/main/java/com/inteligenciadigital/codeblog/controllers/CodeblogController.java
@@ -4,6 +4,7 @@ import com.inteligenciadigital.codeblog.models.Post;
 import com.inteligenciadigital.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,9 +19,17 @@ public class CodeblogController {
 
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = this.codeblogService.findAll();
 		mv.addObject("posts", posts);
+		return mv;
+	}
+
+	@RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = this.codeblogService.findById(id);
+		mv.addObject("post", post);
 		return mv;
 	}
 }
